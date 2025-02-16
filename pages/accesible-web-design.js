eef98ec366eb24bbcf36b1851aa1a29622558b32
BrunoBaumgartner78 <bruno@brainbloom.ch>
2025-02-16 20:09:37 +0100

    Added missing CSS files

--- pages/accesible-web-design.js
+++ pages/accesible-web-design.js
@@ -4,10 +4,10 @@ import React from 'react';
 import CookieBanner from '../src/app/components/cookieBanner';
 import Menu from '@/app/components/menu';
 import PhotoCarousel0 from '@/app/components/photoCaruselA';
-import '../../src/app/styles/global.css';
-import '../../src/app/styles/home.css';
-import '../../src/app/styles/about-us.css'; // Stelle sicher, dass du eine CSS-Datei für das Styling hast
-import '../../src/app/styles/print.css';
+import '../src/app/styles/global.css';
+import '../src/app/styles/home.css';
+import '../src/app/styles/about-us.css'; // Stelle sicher, dass du eine CSS-Datei für das Styling hast
+import '../src/app/styles/print.css';
 
 const AccessibleWebDesign = () => {
   
